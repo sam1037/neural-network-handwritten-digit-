d38b5de2c7404233d40c740e3d01ad7747a33985
sam1037 <samngai2522@gmail.com>
2023-12-21 16:30:30 +0800

    updated get_user_drawing.py with better input format understanding

--- get_user_drawing.py
+++ get_user_drawing.py
@@ -4,11 +4,13 @@ import tensorflow as tf
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import PIL
 
 root = tk.Tk()
 
 #load the model
 model = tf.keras.models.load_model("test model for handwritten")
+#model = tf.keras.models.load_model('floder/mnist.h5')
 
 
 #set the size
@@ -18,10 +20,10 @@ root.title("some randome title")
 
 
 #the canvas
-canvas = tk.Canvas(root, width=600, height=600, highlightthickness=2, highlightbackground="black")
+canvas = tk.Canvas(root, width=600, height=600, highlightthickness=2, highlightbackground="black", bg="white")
 canvas.pack()
 
-canvas.bind("<B1-Motion>", lambda event: canvas.create_oval(event.x-20, event.y-20, event.x+20, event.y+20, fill='black'))
+canvas.bind("<B1-Motion>", lambda event: canvas.create_oval(event.x-15, event.y-15, event.x+15, event.y+15, fill='black'))
 
 
 #clear the drawing
@@ -30,6 +32,7 @@ def clear_drawing():
 
 clear_button = tk.Button(root, text="Clear Drawing", command=clear_drawing)
 clear_button.pack()
+
 
 
 #save the drawing without any normalization
@@ -51,15 +54,40 @@ def predict_digit():
     #normalize the image
     image = cv2.imread("user_drawing.png")
     image = cv2.resize(image, (28,28))
+
+
     image = image[:,:,0]
-    #threshold = 240
+    #print(image.shape, "SHAOPEEEE")
+    
+    threshold = 240
     image = np.invert(np.array([image]))
-    #image = np.where(image >= threshold, 255, 0)
-    image = image/255
-    print(image, type(image))
+    image = np.where(image >= threshold, 1, 0)
+    print(image, image.shape)
+
+
+    #image = image/255.0
+    #image = np.where(image >= threshold, 1, 0)
+    #print(image, type(image))
+    #print(image[0], type(image[0]))
+
+    #image = image[0]
+
+    #image processing
 
     #prediction
     prediction = model.predict(image)
+    print(prediction.shape, prediction)
+    prediction = prediction[0]
+    print(prediction.shape, prediction, type(prediction))
+    for i in prediction:
+        print(i)
+
+    print()
+    print(prediction.max())
+    print(np.sum(prediction))
+    print(np.argmax(prediction))
+
+
     confidence_score = prediction.max()/ np.sum(prediction)
     print(f"The prediction for user drawing is {np.argmax(prediction)} with confidence {confidence_score}")
     plt.imshow(image[0], cmap = plt.cm.binary)
--- train_model.py
+++ train_model.py
@@ -9,19 +9,44 @@ print("hi")
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data() #x is the image, y is the digit
 
+print(x_train.shape, y_train.shape)
+
 #normailize the image
-x_train = tf.keras.utils.normalize(x_train, axis = 1)
-x_test = tf.keras.utils.normalize(x_test, axis = 1)
+#x_train = tf.keras.utils.normalize(x_train, axis = 1)
+#x_test = tf.keras.utils.normalize(x_test, axis = 1)
+
+x_train = np.where(x_train>=100, 1, 0)
+x_test = np.where(x_test>=100, 1, 0)
+
+for x in x_train[0:1]:
+    print(x, type(x), x.shape)   
+
 
 #build and train the model
+"""
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape = (28,28))) #add the input layer
+model.add(tf.keras.layers.Dense(20, activation="relu")) #hidden layer
 model.add(tf.keras.layers.Dense(128, activation="relu")) #hidden layer
 model.add(tf.keras.layers.Dense(128, activation="relu")) #hidden layer
-model.add(tf.keras.layers.Dense(10, activation="softmax")) #output layer
+model.add(tf.keras.layers.Dense(10, activation="softmax")) #output layer"""
 
-model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics= ["accuracy"])
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape = (28,28)),
+    tf.keras.layers.Dense(20, activation="relu"),
+    tf.keras.layers.Dense(20, activation="relu"),
+    tf.keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss=tf.keras.losses.SparseCategoricalCrossentropy(), metrics= ["accuracy"])
 
 model.fit(x_train, y_train, epochs = 3)
 
 model.save("test model for handwritten")
+
+loss, accuracy = model.evaluate(x_test, y_test)
+print(f"loss: {loss}, accuracy: {accuracy}")
+
+prediction = model.predict(x_test)
+print(x_test.shape)
+print(prediction[0], type(prediction))
